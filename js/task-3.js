e83a46cb797f1bec8e17e8552e99f71ed1128212
unknown <voroshnin.s@gmail.com>
2024-01-22 00:41:37 +0200

    Задача 3

--- js/task-3.js
+++ js/task-3.js
@@ -1,15 +1,36 @@
+class StringBuilder {
+    #value
 
+    constructor(initialValue) {
+        this.#value = initialValue;
+    }
 
+    getValue() {
+        return this.#value;
+    }
 
+    padStart(initialValue) {
+        return this.#value = initialValue + this.#value;
+    }
 
+    padEnd(initialValue) {
+        return this.#value = this.#value + initialValue;
+    }
 
+    padBoth(initialValue) {
+        return this.#value = initialValue + this.#value + initialValue;
+    }
+}
 
 
 const builder = new StringBuilder(".");
 console.log(builder.getValue()); // "."
+
 builder.padStart("^");
 console.log(builder.getValue()); // "^."
+
 builder.padEnd("^");
 console.log(builder.getValue()); // "^.^"
+
 builder.padBoth("=");
 console.log(builder.getValue()); // "=^.^="
