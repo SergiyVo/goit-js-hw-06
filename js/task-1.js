d37da1871d49dd8724937200b57785787084fca6
unknown <voroshnin.s@gmail.com>
2024-01-22 00:21:10 +0200

    Update task-1.js

--- js/task-1.js
+++ js/task-1.js
@@ -27,6 +27,7 @@ const customer = {
 
 customer.setDiscount(0.15);
 console.log(customer.getDiscount()); // 0.15
+
 customer.addOrder(5000, "Steak");
 console.log(customer.getBalance()); // 19750
 console.log(customer.getOrders()); // ["Burger", "Pizza", "Salad", "Steak"]
